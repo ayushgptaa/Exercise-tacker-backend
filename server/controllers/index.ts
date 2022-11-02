5a5f991c9ce33c565cd8a07bcbea4715fed5042c
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-11-02 22:54:19 +0530

    📦 NEW:  addaddExercise +   removeExercise routes

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,4 +1,3 @@
-import mongoose from "mongoose"
 import { Request, Response } from "express"
 
 import WorkoutModel from "../models"
@@ -29,10 +28,8 @@ const getSingleWorkout = async (req: Request, res: Response) => {
 
 // Create a new workout
 const createNewWorkout = async (req: Request, res: Response) => {
-  const { title, exercises } = req.body
-
   try {
-    const workout = await WorkoutModel.create({ title, exercises })
+    const workout = await WorkoutModel.create({ ...req.body })
     return res.status(200).json(workout)
   } catch (error) {
     if (error instanceof Error) {
@@ -50,7 +47,7 @@ const deleteWorkout = async (req: Request, res: Response) => {
     return res.status(400).json({ error: "Invalid workout ID" })
   }
 
-  const workout = await WorkoutModel.findByIdAndDelete({ _id: id })
+  const workout = await WorkoutModel.findByIdAndDelete(id)
 
   if (!workout) {
     return res.status(404).json({ error: "Workout not found" })
@@ -76,4 +73,42 @@ const updateWorkout = async (req: Request, res: Response) => {
   return res.status(200).json({ msg: "Workout updated" })
 }
 
-export { createNewWorkout, getAllWorkouts, getSingleWorkout, deleteWorkout, updateWorkout }
+//Add exercise to workout
+const addExercise = async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  if (!isValidId(id)) {
+    return res.status(400).json({ error: "Invalid workout ID" })
+  }
+
+  const workout = await WorkoutModel.findByIdAndUpdate(
+    id,
+    { $addToSet: { exercises: { ...req.body } } },
+    { new: true, upsert: true, safe: true }
+  )
+
+  if (!workout) {
+    return res.status(404).json({ error: "Workout not found" })
+  }
+
+  return res.status(200).json({ msg: "Workout updated" })
+}
+
+//Remove exercise from a workout
+const removeExercise = async (req: Request, res: Response) => {
+  const { id } = req.params
+
+  if (!isValidId(id)) {
+    return res.status(400).json({ error: "Invalid workout ID" })
+  }
+
+  const workout = await WorkoutModel.findByIdAndUpdate(id, { $pull: { exercises: { ...req.body } } })
+
+  if (!workout) {
+    return res.status(404).json({ error: "Workout not found" })
+  }
+
+  return res.status(200).json({ msg: "Workout updated" })
+}
+
+export { createNewWorkout, getAllWorkouts, getSingleWorkout, deleteWorkout, updateWorkout, addExercise, removeExercise }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,15 @@
 import { Router } from "express"
+import WorkoutModel from "../models"
 
-import { createNewWorkout, getAllWorkouts, getSingleWorkout, deleteWorkout, updateWorkout } from "../controllers"
+import {
+  createNewWorkout,
+  getAllWorkouts,
+  getSingleWorkout,
+  deleteWorkout,
+  updateWorkout,
+  addExercise,
+  removeExercise
+} from "../controllers"
 
 const router = Router()
 
@@ -18,5 +27,17 @@ router.delete("/:id", deleteWorkout)
 
 //UPDATE new workout
 router.patch("/:id", updateWorkout)
+
+//Add exercise to workout
+router.patch("/:id/exercises", addExercise)
+
+//Add exercise to workout
+router.delete("/:id/exercises", removeExercise)
+
+//Temp route to delete all workouts
+router.delete("/", async (req, res) => {
+  await WorkoutModel.deleteMany({})
+  res.status(200).json({ msg: "All workouts deleted" })
+})
 
 export default router
